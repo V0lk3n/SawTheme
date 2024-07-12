6b9b4cfb0f1f2301f6990651cb9c52b9573c3787
V0lk3n <russo.kevin@protonmail.com>
2024-07-12 19:47:21 +0200

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -2,4 +2,4 @@
 powershell.exe -Command "Start-BitsTransfer -Source 'https://github.com/V0lk3n/W11-SAWTheme/releases/download/Pre-Release/SawTheme.zip' -Destination 'C:\Users\$env:UserName\Desktop\SawTheme.zip'"
 
 # Extract Theme and run installer
-powershell.exe -Command "Start-Process powershell.exe -ArgumentList '-Command Expand-Archive -Path C:\Users\$env:UserName\Desktop\SawTheme.zip -DestinationPath C:\Windows\Resources\Themes;C:\Windows\Resources\Themes\SawTheme\Setup\SawTheme.ps1' -Verb RunAs"
+powershell.exe -Command "Start-Process powershell.exe -ArgumentList '-noprofile', '-exec', 'Bypass', '-Command Expand-Archive -Path C:\Users\$env:UserName\Desktop\SawTheme.zip -DestinationPath C:\Windows\Resources\Themes;C:\Windows\Resources\Themes\SawTheme\Setup\SawTheme.ps1' -Verb RunAs"
